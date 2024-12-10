93038e0a2cc11f8cc875be097b09c1ae948fb742
nbrouand <7816908+nbrouand@users.noreply.github.com>
2024-12-10 10:32:41 +0100

    WIP

---
+++ chutney/server/src/test/java/blackbox/SsoTest.java
@@ -0,0 +1,86 @@
+/*
+ * SPDX-FileCopyrightText: 2017-2024 Enedis
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package blackbox;
+
+import static java.util.Optional.ofNullable;
+import static org.springframework.http.HttpMethod.DELETE;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.PATCH;
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.HttpMethod.PUT;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.NOT_IMPLEMENTED;
+import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.anonymous;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.request;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.chutneytesting.ServerConfiguration;
+import com.chutneytesting.security.api.UserDto;
+import com.chutneytesting.tools.file.FileUtils;
+import java.io.File;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@SpringBootTest(classes = {ServerConfiguration.class})
+@TestPropertySource(properties = "spring.datasource.url=jdbc:h2:mem:testdbsecu")
+@TestPropertySource(properties = "spring.config.location=classpath:blackbox/")
+public class SsoTest {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mockMvc;
+
+    @BeforeAll
+    public static void cleanUp() {
+        FileUtils.deleteFolder(new File("./target/.chutney").toPath());
+    }
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders
+            .webAppContextSetup(context)
+            .apply(springSecurity())
+            .build();
+    }
+    //  {GET, "/api/scenario/v2", "SCENARIO_READ", null, OK},
+    @Test
+    void givenAnonymousUser_whenGetOnProtectedApi_thenRedirectToLoginPage() throws Exception {
+        mockMvc.perform(
+            get("/login")
+                .with(anonymous())
+                .secure(true)
+                .accept(MediaType.APPLICATION_JSON)
+            ).andDo(print())
+            .andExpect(status().isOk() );
+    }
+}
