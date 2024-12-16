59456add0d5f93a00e071c1891c793806568bbd8
k-sauvee <kylian.sauvee@enedis.fr>
2024-12-16 17:25:48 +0100

    feat(action): Replace Integer type by Number to support Integer, Double, Float and Long in math spel functions

--- chutney/action-impl/src/main/java/com/chutneytesting/action/function/MathFunctions.java
+++ chutney/action-impl/src/main/java/com/chutneytesting/action/function/MathFunctions.java
@@ -12,17 +12,49 @@ import com.chutneytesting.action.spi.SpelFunction;
 public class MathFunctions {
 
     @SpelFunction
-    public static Integer abs(Integer a) {
-        return Math.abs(a);
+    public static Number abs(Number a) {
+        String numberType = a.getClass().getSimpleName();
+
+        return switch (numberType) {
+            case "Double" -> Math.abs(a.doubleValue());
+            case "Float" -> Math.abs(a.floatValue());
+            case "Integer" -> Math.abs(a.intValue());
+            case "Long" -> Math.abs(a.longValue());
+            default -> throw new IllegalArgumentException("Unexpected type: " + numberType);
+        };
     }
 
     @SpelFunction
-    public static Integer min(Integer a, Integer b) {
-        return Math.min(a, b);
+    public static Number min(Number a, Number b) {
+        String numberType = a.getClass().getSimpleName();
+
+        if (!numberType.equals(b.getClass().getSimpleName())) {
+            throw new IllegalArgumentException("Type is not consistent");
+        }
+
+        return switch (numberType) {
+            case "Double" -> Math.min(a.doubleValue(), b.doubleValue());
+            case "Float" -> Math.min(a.floatValue(), b.floatValue());
+            case "Integer" -> Math.min(a.intValue(), b.intValue());
+            case "Long" -> Math.min(a.longValue(), b.longValue());
+            default -> throw new IllegalArgumentException("Unexpected type: " + numberType);
+        };
     }
 
     @SpelFunction
-    public static Integer max(Integer a, Integer b) {
-        return Math.max(a, b);
+    public static Number max(Number a, Number b) {
+        String numberType = a.getClass().getSimpleName();
+
+        if (!numberType.equals(b.getClass().getSimpleName())) {
+            throw new IllegalArgumentException("Type is not consistent");
+        }
+
+        return switch (numberType) {
+            case "Double" -> Math.max(a.doubleValue(), b.doubleValue());
+            case "Float" -> Math.max(a.floatValue(), b.floatValue());
+            case "Integer" -> Math.max(a.intValue(), b.intValue());
+            case "Long" -> Math.max(a.longValue(), b.longValue());
+            default -> throw new IllegalArgumentException("Unexpected type: " + numberType);
+        };
     }
 }
--- chutney/action-impl/src/test/java/com/chutneytesting/action/function/MathFunctionsTest.java
+++ chutney/action-impl/src/test/java/com/chutneytesting/action/function/MathFunctionsTest.java
@@ -8,23 +8,65 @@
 package com.chutneytesting.action.function;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
 class MathFunctionsTest {
 
     @Test
+    void abs_unexpected_type() {
+        assertThrows(
+            IllegalArgumentException.class,
+            () -> {
+                short a = 2;
+                MathFunctions.abs(a);
+            }
+        );
+    }
+
+    @Test
     void abs_minus_2_should_return_2() {
+        assertThat(MathFunctions.abs(-2d)).isEqualTo(2d);
+        assertThat(MathFunctions.abs(-2f)).isEqualTo(2f);
         assertThat(MathFunctions.abs(-2)).isEqualTo(2);
+        assertThat(MathFunctions.abs(-2L)).isEqualTo(2L);
+    }
+
+    @Test
+    void min_unexpected_type() {
+        assertThrows(
+            IllegalArgumentException.class,
+            () -> {
+                MathFunctions.min(4f, 9);
+            }
+        );
     }
 
     @Test
     void min_4_and_9_should_return_4() {
+        assertThat(MathFunctions.min(4d, 9d)).isEqualTo(4d);
+        assertThat(MathFunctions.min(4f, 9f)).isEqualTo(4f);
         assertThat(MathFunctions.min(4, 9)).isEqualTo(4);
+        assertThat(MathFunctions.min(4L, 9L)).isEqualTo(4L);
     }
 
     @Test
+    void max_unexpected_type() {
+        assertThrows(
+            IllegalArgumentException.class,
+            () -> {
+                MathFunctions.max(4f, 9);
+            }
+        );
+    }
+
+
+    @Test
     void max_4_and_9_should_return_9() {
+        assertThat(MathFunctions.max(4d, 9d)).isEqualTo(9d);
+        assertThat(MathFunctions.max(4f, 9f)).isEqualTo(9f);
         assertThat(MathFunctions.max(4, 9)).isEqualTo(9);
+        assertThat(MathFunctions.max(4L, 9L)).isEqualTo(9L);
     }
 }
